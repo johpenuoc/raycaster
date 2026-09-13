b6ca239c6ec4f56c2fb4e2b7dea3f94a75730f4e
johpenuoc <325361506+johpenuoc@users.noreply.github.com>
2026-09-13 19:05:46 +0100

    -last init

---
+++ src/core/main.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.locals import *
+import sys 
+
+class Main:
+    def __init__(self):
+        self.window = pygame.display.set_mode((640, 360))
+        self.display = pygame.Surface((320, 180))
+        self.clock = pygame.Clock()
+        self.dt = 0
+
+    def run(self):
+        while 1:
+            self.dt = self.clock.tick(60) / 1000
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.display.fill((0, 0, 0))
+
+            self.window.blit(
+                pygame.transform.scale(self.display, (640, 360)), (0, 0)
+            )
+            self.window.update()
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
